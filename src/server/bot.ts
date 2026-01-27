c984a9e2040891bdac830ab55a462fdc26f54a11
Édipo <edipo1998@gmail.com>
2026-01-27 11:14:00 -0300

    fix /joao

--- src/server/bot.ts
+++ src/server/bot.ts
@@ -63,10 +63,10 @@ export class BotServer extends IBotServerPort {
         ],
         timeout: 120000,
       },
-      /* webVersionCache: {
+      webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-      }, */
+      },
     });
   }
 
